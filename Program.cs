7750e011b15e56383ca23d686c3f676e967f9954
Matt Owens <matthewo@quantium.com>
2021-06-19 22:53:06 +1000

    inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pkg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
